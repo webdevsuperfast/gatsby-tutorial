34e843d1222726898488fad6a97afb7b49a75b50
webdevsuperfast <acob.rotsen@hotmail.com>
2019-09-24 13:52:16 +0800

    Added code based on tutorial

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,5 +18,19 @@ module.exports = {
         pathToConfigModule: `src/utils/typography`,
       },
     },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `GatsbyJS`,
+        short_name: `GatsbyJS`,
+        start_url: `/`,
+        background_color: `#6b37bf`,
+        theme_color: `#6b37bf`,
+        display: `standalone`,
+        icon: `src/images/icon.png`
+      },
+    },
+    `gatsby-plugin-offline`,
+    `gatsby-plugin-react-helmet`
   ],
 }
